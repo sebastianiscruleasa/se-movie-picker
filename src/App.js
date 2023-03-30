755115dc0088875022479ab0eb7eeb3f779bbee4
Sebastian Iscruleasa <sebi.iscruleasa@yahoo.ro>
2023-03-30 22:22:09 +0300

    first draft of the home page with a background image moved BrowserRouter to the index.js file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import './App.css';
-import HomePage from "./pages/HomePage";
-import QuestionPage from "./pages/QuestionPage";
+import HomePage from "./pages/HomePage/HomePage";
+import QuestionPage from "./pages/QuestionPage/QuestionPage";
 import { Route, Routes } from "react-router-dom";
-import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
         <Routes>
             <Route path="/" element={<HomePage/>} />
             <Route path="/question" element={<QuestionPage/>} />
         </Routes>
-      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,10 @@
+import styles from "./HomePage.module.css";
+
+function HomePage() {
+  return (
+    <div className={styles.background}>
+    </div>
+  );
+}
+
+export default HomePage;
