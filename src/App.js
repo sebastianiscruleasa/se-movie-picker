13655d0efbaff71795b51a7d1bff1cfdc8fe780b
Sebastian Iscruleasa <sebi.iscruleasa@yahoo.ro>
2023-03-30 21:53:50 +0300

    created dummy pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import HomePage from "./pages/HomePage";
+import QuestionPage from "./pages/QuestionPage";
+import { Route, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      First Commit
+      <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<HomePage/>} />
+            <Route path="/question" element={<QuestionPage/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/QuestionPage.js
@@ -0,0 +1,9 @@
+function QuestionPage() {
+    return (
+        <div>
+            <h1>Question Page</h1>
+        </div>
+    );
+}
+
+export default QuestionPage;
