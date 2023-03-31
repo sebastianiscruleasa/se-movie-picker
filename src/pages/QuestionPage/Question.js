fb5b9db2de9b27439630fedb2ee70a1bb84226ac
Sebastian Iscruleasa <sebi.iscruleasa@yahoo.ro>
2023-03-31 19:47:08 +0300

    created the question component and created a more realistic draft of the QuestionPage implemented the logic to get the response for every question and then displaying the next question using some dummy data

---
+++ src/pages/QuestionPage/Question.js
@@ -0,0 +1,16 @@
+import styles from "./Question.module.css";
+import Button from "../../components/Button/Button";
+
+function Question({question, handleResponse}) {
+    return (
+        <div className={styles.questionContainer}>
+            <div className={styles.questionText}>{question.question}</div>
+            <div className={styles.optionsContainer}>
+                <Button buttonName={question.option1} onClick={() => handleResponse('0')}/>
+                <Button buttonName={question.option2} onClick={() => handleResponse('1')}/>
+            </div>
+        </div>
+    );
+}
+
+export default Question;
--- src/pages/QuestionPage/QuestionPage.js
+++ src/pages/QuestionPage/QuestionPage.js
@@ -1,7 +1,55 @@
+import styles from './QuestionPage.module.css';
+import Question from "./Question";
+import {useState} from "react";
+
+const questions = {
+    "Q01":
+        {
+            question: "What is the capital of France?",
+            option1: "Paris1",
+            option2: "London1"
+        },
+    "Q02":
+        {
+            question: "What is the capital of England?",
+            option1: "Paris2",
+            option2: "London2"
+        },
+    "Q03":
+        {
+            question: "What is the capital of Germany?",
+            option1: "Paris3",
+            option2: "London3"
+        }
+}
+
 function QuestionPage() {
+    const [questionIndex, setQuestionIndex] = useState(1);
+    const [responses, setResponses] = useState({});
+
+    const handleResponse = (response) => {
+        if (questionIndex < 3) {
+            setResponses((prevState) => {
+                    const questionId = `Q0${questionIndex}`;
+                    return {...prevState, [questionId]: response}
+                }
+            );
+            setQuestionIndex((prevState) => prevState + 1);
+        } else {
+            setResponses((prevState) => {
+                    const questionId = `Q0${questionIndex}`;
+                    return {...prevState, [questionId]: response}
+                }
+            );
+            setQuestionIndex((prevState) => prevState + 1);
+            //call inference machine
+        }
+    }
+
     return (
-        <div>
-            <h1>Question Page</h1>
+        <div className={styles.background}>
+            {questionIndex <=3 &&<Question question={questions[`Q0${questionIndex}`]} handleResponse={handleResponse}/>}
+            {questionIndex > 3 && <div>Thank you for your responses</div>}
         </div>
     );
 }
