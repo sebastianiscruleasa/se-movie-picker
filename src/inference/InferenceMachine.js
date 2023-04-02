e75057467b920dbec700812baf2c6bfd638097a7
Mihai405 <mihaijurj2000@yahoo.com>
2023-04-02 16:55:34 +0300

    Empty response

--- src/inference/InferenceMachine.js
+++ src/inference/InferenceMachine.js
@@ -41,5 +41,14 @@ export async function getMovieFromQuestions(response) {
   let fq4Movies = runtimeFilter(fq3Movies, response[4]);
   let fq5Movies = languageFilter(fq4Movies, response[5]);
 
-  return fq5Movies[Math.floor(Math.random() * fq5Movies.length)];
+  if (fq5Movies.length !== 0) {
+    return fq5Movies[Math.floor(Math.random() * fq5Movies.length)];
+  } else {
+    return {
+      title: "No movies found based on your preferences",
+      imdbrating: 0,
+      poster:
+        "https://i.pinimg.com/originals/fd/d5/f2/fdd5f2754304c3e16bfd2788f666c9d1.gif",
+    };
+  }
 }
